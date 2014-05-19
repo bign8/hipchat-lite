c68cd28bfb679938a3fdd2010fa5cab808ce9d27
Nathan Woods <big.nate.w@gmail.com>
2014-05-18 23:38:25 -0600

    Multi-room chat (must manually emit join and leave events)

--- server.js
+++ server.js
@@ -1,14 +1,65 @@
 var express = require('express'), app = express(), 
-  server = require('http').createServer(app).listen(80),
-  io = require('socket.io').listen(server, {
-  	'log level': 2
-  });
+	server = require('http').createServer(app).listen(80),
+	io = require('socket.io').listen(server, {
+		'log level': 2
+	}),
+	store = require('./lib/store.js');
+
+// Data storage
+Viewing = {}; // in ram (built from user-meta) (read from db on connect)
+// Messages = {}; // periodically backed up to files
 
 app.use( express.static(__dirname + '/www') );
 
 io.sockets.on('connection', function (socket) {
-  socket.on('message', function (data) {
-  	socket.broadcast.emit('message', data);
-  });
-  socket.broadcast.emit('message', 'user connected');
+
+	// Join and Leave Rooms!
+	socket.on('join', function ( roomID ) {
+		if ( Viewing.hasOwnProperty(roomID) ) {
+			Viewing[roomID].push(socket.id);
+		} else {
+			Viewing[roomID] = [socket.id];
+		}
+	});
+	socket.on('leave', function ( roomID ) {
+		var idx = Viewing[roomID].indexOf( socket.id );
+		Viewing[roomID].splice(idx, 1);
+	});
+	socket.on('message', function (data) {
+		socket.get('user', function (err, user) {
+			data.user_id = user.user_id;
+			data.name = user.name;
+			if (Viewing.hasOwnProperty(data.roomID)) 
+				for (var i = 0, len = Viewing[data.roomID].length; i < len; i++) 
+					io.sockets.socket( Viewing[data.roomID][i] ).emit('message', data);
+			console.log(data);
+		});
+	});
+
+	// API data gathering calls
+	socket.on('history', function (data, cb) {
+		cb(['blank']);
+	});
+	socket.on('rooms', function (data, cb) {
+		cb(['Project', 'Room', 'Private']);
+	});
+
+	// Managing user statuses
+	socket.on('status', function (status) {
+		socket.get('user', function (err, user) {
+			user.status = status;
+			socket.broadcast.emit('status', user);
+		});
+	});
+	socket.on('disconnect', function () {
+		socket.get('user', function (err, user) {
+			user.status = false;
+			socket.broadcast.emit('status', user);
+		});
+	});
+	store.getUserFromIP(socket.handshake.address.address, function (user) { // Init user logon
+		socket.set('user', user, function () {
+			socket.broadcast.emit('status', user);
+		});
+	});
 });
--- www/js.js
+++ www/js.js
@@ -2,20 +2,56 @@ var socket = io.connect('http://localhost'),
 	output = document.getElementById('message_box'),
 	form = document.chat;
 
+// Messsage connections
+socket.on('connect', function() {
+	socket.emit('join', 0);
+});
 socket.on('message', function (data) {
-	var ele = document.createElement('div');
-	ele.innerHTML = data;
-	output.appendChild(ele);
+	add_msg(data.msg, data.name, data.stamp);
 });
 
+// Sending messages
+var Active_Room = 0;
 form.addEventListener('submit', function (e){
 	if (e.preventDefault) e.preventDefault();
-
-	// if (form.name.value == '') return this.add_msg('Enter your Name please!');
-	if (form.message.value == '') return this.add_msg('Enter a message please!');
-
-	socket.emit('message', form.message.value);
-
-	form.message.value = ''; //reset text
+	if (form.message.value != '') socket.emit('message', {
+		roomID: Active_Room,
+		msg: form.message.value,
+		stamp: (new Date()).toISOString(),
+	});
+	form.message.value = '';
 	return false;
 });
+
+// Adding message
+var add_msg = function (msg, name, time) {
+	time = new Date(time);
+	time = time.toLocaleTimeString(undefined, {hour:'numeric', minute:'2-digit'});
+	name = name ? name : 'Unknown';
+	msg = parse_msg(msg);
+
+	var last = output.lastChild;
+	if (
+		last &&
+		last.querySelector('.nick').innerHTML == name &&
+		last.querySelector('.time').innerHTML == time
+	) {
+		last.querySelector('.msg').innerHTML += '<br/>' + msg;
+	} else {
+		var ele = document.createElement('div');
+		ele.innerHTML = '<div class="nick">' + name + '</div>';
+		ele.innerHTML += '<div class="msg"><div class="time">' + time + '</div>' +  msg + '</div>';
+		output.appendChild(ele);
+	}
+};
+
+// Message parsing
+var parse_msg = function (msg) {
+	// http://stackoverflow.com/questions/3809401/what-is-a-good-regular-expression-to-match-a-url
+	var regex = /(https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&\/\/=]*))/;
+
+	msg = msg.replace(regex, function (rep) {
+		return '<a href="' + rep + '" target="_blank">' + rep + '</a>';
+	});
+	return msg;
+};
